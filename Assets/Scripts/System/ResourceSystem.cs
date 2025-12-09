509f39ad535ee59359de10f2ce7f5142ef76c263
Vaizted <jonathansoderman@gmail.com>
2025-12-09 23:28:04 +0100

    Update ResourceSystem.cs

--- Assets/Scripts/System/ResourceSystem.cs
+++ Assets/Scripts/System/ResourceSystem.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 
 public class ResourceSystem : Singleton<ResourceSystem>
 {
-    public List<ScriptableUnitBase> Units { get; private set; }
-    private Dictionary<UnitType, ScriptableUnitBase> UnitDict;
-
-    protected override void Awake()
-    {
-        base.Awake();
-        AssembleResources();
-    }
-
-    private void AssembleResources()
-    {
-        Units = Resources.LoadAll<ScriptableUnitBase>("Cows").ToList();
-        UnitDict = Units.ToDictionary(r => r.UnitType, r => r);
-    }
-    
-    public ScriptableUnitBase GetUnit(UnitType type) => UnitDict[type];
-    public ScriptableUnitBase GetRandomUnit() => Units[Random.Range(0, Units.Count)];
-
+   // public List<> Units { get; private set; }
+   // private Dictionary<UnitType, ScriptableUnitBase> UnitDict;
+   //
+   // protected override void Awake()
+   // {
+   //     base.Awake();
+   //     AssembleResources();
+   // }
+   //
+   // private void AssembleResources()
+   // {
+   //     Units = Resources.LoadAll<ScriptableUnitBase>("Cows").ToList();
+   //     UnitDict = Units.ToDictionary(r => r.UnitType, r => r);
+   // }
+   // 
+   // public ScriptableUnitBase GetUnit(UnitType type) => UnitDict[type];
+   // public ScriptableUnitBase GetRandomUnit() => Units[Random.Range(0, Units.Count)];
+   //
 }
