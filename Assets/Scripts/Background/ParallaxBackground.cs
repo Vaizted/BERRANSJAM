b00c16d183ffbc0fbdb4c081577db58c54a131f7
Jacob Bernharth <jernharth@gmail.com>
2025-12-10 21:38:42 +0100

    Add looping parallax background

--- Assets/Scripts/Background/ParallaxBackground.cs
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -1,12 +1,22 @@
+using System.Collections.Generic;
+using Mono.Cecil;
+using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ParallaxBackground : MonoBehaviour
 {
     [System.Serializable]
     public class ParallaxLayer
     {
-        public Transform layerTransform;
-        public float parallaxFactor;
+        public Transform transform;
+        public SpriteRenderer renderer;
+        public float parallaxSpeed;
+
+        private List<GameObject> activeSegments = new List<GameObject>();
+        public List<GameObject> ActiveSegments => activeSegments;
+        public float SpriteWidth => renderer.bounds.size.x;
     }
 
     public ParallaxLayer[] layers;
@@ -15,6 +25,11 @@ public class ParallaxBackground : MonoBehaviour
 
     void Start()
     {
+        foreach (ParallaxLayer layer in layers)
+        {
+            InitializeLayer(layer);
+        }
+
         cam = Camera.main.transform;
         previousCamPos = cam.position;
     }
@@ -25,12 +40,39 @@ public class ParallaxBackground : MonoBehaviour
 
         foreach (ParallaxLayer layer in layers)
         {
-            Vector3 newPos = layer.layerTransform.position;
-            newPos += deltaMovement * layer.parallaxFactor;
-            layer.layerTransform.position = newPos;
+            UpdatePosition(layer, deltaMovement);
         }
 
         previousCamPos = cam.position;
+    }
+
+    private void UpdatePosition(ParallaxLayer layer, Vector3 deltaMovement)
+    {
+        Vector3 newPos = layer.transform.position;
+        newPos += deltaMovement * layer.parallaxSpeed;
+        layer.transform.position = newPos;
+    }
+
+    private void InitializeLayer(ParallaxLayer layer)
+    {
+        float spriteWidth = layer.SpriteWidth;
+        int loopsNeeded = LoopsNeeded(spriteWidth);
+
+        float screenWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect / 2;
+
+        for (int i = 0; i < loopsNeeded; i++)
+        {
+            Vector3 pos = layer.transform.position - new Vector3(screenWidth, 0, 0) + Vector3.right * spriteWidth * i;
+            GameObject segment = Instantiate(layer.renderer.gameObject, pos - Vector3.left, Quaternion.identity, layer.transform);
+            layer.ActiveSegments.Add(segment);
+        }
+    }
+
+    private int LoopsNeeded(float spriteWidth) 
+    {
+        float screenWidth = Camera.main.orthographicSize * 2f * Camera.main.aspect;
+        float margin = spriteWidth * 2f;
+        return Mathf.CeilToInt((screenWidth + margin) / spriteWidth);
     }
 }
 
